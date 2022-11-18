0a19de8ea16975e1ac09805102d0934ed102f9e9
Star <Star@DESKTOP-6A3180H>
2022-11-18 10:13:51 +0530

    first commit

---
+++ Corejava-B35/src/Datatypes/datatypes.java
@@ -0,0 +1,28 @@
+package Datatypes;
+
+public class datatypes {
+
+	public static void main(String[] arg) {
+		//All primitive and non-primitive datatypes
+		int x=143456;
+		long a=123456789L;
+		char grade='C';
+		float num=3.111f;
+		double value=3.1111111111;
+		String name="Anusha";
+		byte d=125;
+		short c=32767;
+		boolean z=true; 
+		System.out.println("the value is:" +x);
+		System.out.println("the value is:"+a);
+		System.out.println("the value is:"+grade);
+		System.out.println("the value is:"+num);
+		System.out.println("the value is:"+value);
+		System.out.println("the value is:"+name);
+		System.out.println("the value is:"+d);
+		System.out.println("the value is:"+c);
+		System.out.println("the value is:"+z);
+		
+	}
+
+}
